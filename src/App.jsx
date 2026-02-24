2e48287d58966f5c4ef3dd64a72ce2026a4f4490
joseah2004 <joseah2004@gmail.com>
2026-02-24 12:02:22 +0100

    feat: ajustes finales del login

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
 import { useState } from 'react';
-import './App.css'
-import Sidebar from './componentes/sidebar';
+import './App.css';
+import Sidebar from './componentes/Sidebar';
 import Inicio from './componentes/Inicio';
+import Login from './componentes/Login';
 
 function App() {
+  const [logueado, setLogueado] = useState(false);
   const [vistaActual, setVistaActual] = useState('Inicio');
 
+  // Si NO está logueado → mostrar login
+  if (!logueado) {
+    return <Login onLogin={() => setLogueado(true)} />;
+  }
+
+  // Si está logueado → mostrar dashboard
   const renderizarContenido = () => {
-    switch(vistaActual) {
+    switch (vistaActual) {
       case 'Inicio':
         return <Inicio />;
       case 'Usuarios':
-        return <div className='p-8 text-white'><h1 className='text-3xl'>Usuarios</h1></div>;
+        return (
+          <div className='p-8 text-white'>
+            <h1 className='text-3xl'>Usuarios</h1>
+          </div>
+        );
       case 'Alertas':
-        return <div className='p-8 text-white'><h1 className='text-3xl'>Alertas</h1></div>;
+        return (
+          <div className='p-8 text-white'>
+            <h1 className='text-3xl'>Alertas</h1>
+          </div>
+        );
       case 'Infraestructura':
-        return <div className='p-8 text-white'><h1 className='text-3xl'>Infraestructura</h1></div>;
+        return (
+          <div className='p-8 text-white'>
+            <h1 className='text-3xl'>Infraestructura</h1>
+          </div>
+        );
       default:
         return <Inicio />;
     }
   };
 
   return (
-     <div className="flex">
+    <div className="flex">
       <Sidebar cambiarVista={setVistaActual} vistaActual={vistaActual} />
       <div className="flex-1 min-h-screen bg-gray-950">
         {renderizarContenido()}
       </div>
-     </div>
+    </div>
   );
 }
 
-export default App
+export default App;
